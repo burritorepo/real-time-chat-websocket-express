e853bbb90d1323660f0b44a3551ccd83e06c1d38
Harold Mantilla <hacodeml@gmail.com>
2019-04-18 23:37:01 -0500

    working chat functionality

--- index.js
+++ index.js
@@ -18,4 +18,8 @@ var io = socket(server);
 
 io.on('connection', function (socket){
     console.log('made socket connection',socket.id);
+    socket.on('chat', function(data) {
+        io.sockets.emit('chat',data);
+    })
 });
+
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,22 @@
 //These are running on the front end
 //Make connection
-var socket = io.connect('http://localhost:4000');
+let socket = io.connect('http://localhost:4000');
+
+//Querying the DOM and creating variables
+let message = document.querySelector('#message'),
+    handle = document.querySelector('#handle'),
+    btn = document.querySelector('#send'),
+    output = document.querySelector('#output');
+
+//Emit events
+btn.addEventListener('click', function() {
+    socket.emit('chat',{
+        message: message.value,
+        handle: handle.value
+    })
+});
+
+//Listen for events
+socket.on('chat',function(data){
+    output.innerHTML += `<p><strong> ${data.handle}: </strong> ${data.message}</p>`
+});
