e9ec77ae865ae86914fae53a51e2243e33fcf984
Harold Mantilla <hacodeml@gmail.com>
2019-04-19 00:07:24 -0500

    add socketbroadcast and new div when user is writing a message

--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,8 @@ let socket = io.connect('http://localhost:4000');
 let message = document.querySelector('#message'),
     handle = document.querySelector('#handle'),
     btn = document.querySelector('#send'),
-    output = document.querySelector('#output');
+    output = document.querySelector('#output'),
+    feedback = document.querySelector('#feedback');
 
 //Emit events
 btn.addEventListener('click', function() {
@@ -16,7 +17,16 @@ btn.addEventListener('click', function() {
     })
 });
 
+message.addEventListener('keypress', function() {
+    socket.emit('typing', handle.value);
+});
+
 //Listen for events
 socket.on('chat',function(data){
-    output.innerHTML += `<p><strong> ${data.handle}: </strong> ${data.message}</p>`
+    output.innerHTML += `<p><strong> ${data.handle}: </strong> ${data.message}</p>`;
+    feedback.innerHTML = "";
 });
+
+socket.on('typing', function(data){
+    feedback.innerHTML = `<p><em> ${data} is typing a message...</em></p>`;
+})
